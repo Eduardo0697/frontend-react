f026ee1b3581a6f3b017e68bcb99329954f54a3e
Eduardo Hernandez <eduardohdz0697@hotmail.com>
2020-03-11 12:41:05 -0600

    Se añadio campo busqueda por email

--- src/Task/ResumeToday.js
+++ src/Task/ResumeToday.js
@@ -6,6 +6,25 @@ function ResumeToday(props){
     console.log(props);
     return(
         <div className="resume-today">
+
+            <div className="my-3">
+                <form>
+                    <div class="form-group justify-content-md-end row mb-0">
+                        <label for="inputEmail3" class="col-md-1 col-form-label">Email</label>
+                        <div class="col-md-4 mb-3">
+                            <input type="email" class="form-control" id="inputEmail3" placeholder="Email"></input>
+                        </div>
+                        <div className="form-group col-md-1">
+                            <button type="submit" class="btn btn-primary"><span><i class="fas fa-search"></i></span></button>
+                        </div>
+                    </div>
+                    <div className="form-group justify-content-center justify-content-md-end row mr-md-3">
+                        <small id="emailHelp" class="form-text text-muted">Please search your tasks using your email</small>
+                    </div> 
+                    
+                </form>
+            </div>
+
             <div className="text-left mb-3">
               <p className="h3">Today Tasks</p>
             </div>
