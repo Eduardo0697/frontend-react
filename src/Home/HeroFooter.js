f8566302b027741a91c34188722108c0c5f2f7be
Eduardo Hernandez <eduardohdz0697@hotmail.com>
2020-03-13 12:03:24 -0600

    Design homepage, icons.

--- src/Home/HeroFooter.js
+++ src/Home/HeroFooter.js
@@ -8,7 +8,7 @@ function HeroFooter(){
     <div className="hero-footer full-width mb-5">
         <div className="hero-text">
           <h1>Now</h1>
-          <p>Star Working on each of your goals and you will succeed!</p>
+          <p className="h3">Star Working on each of your goals and you will succeed!</p>
         </div>
     </div> 
         
